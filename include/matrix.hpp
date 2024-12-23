3786bd6dd586f1554fd5fe5b9646711d276e7025
IroNEDR <TirasEkin@gmail.com>
2024-12-24 00:26:00 +0100

    matrix multiplication & tuples

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,3 +1,4 @@
+#include "tuple.hpp"
 #include <array>
 #include <concepts>
 #include <cstddef>
@@ -34,8 +35,16 @@ public:
         return *this;
     }
 
-    std::size_t size() {
+    constexpr std::size_t size() const {
+        return Rows * Cols;
+    }
 
+    constexpr int rows() const {
+        return Rows;
+    }
+
+    constexpr int cols() const {
+        return Cols;
     }
 
     const std::array<T, Cols> operator[](int row) const {
@@ -81,6 +90,32 @@ public:
             }
         }
         return false;
+    }
+
+    template<int N>
+    // This ensures that the number of columns in the first matrix is equal to the number of rows in the second matrix
+    // and the number of columns in the second matrix can be different but can be known at compile time
+    Matrix<T, Rows, N> operator*(const Matrix<T, Cols, N>& rhs) {
+        Matrix<T, Rows, N> m;
+
+        for(int i {0}; i < Rows; ++i) {
+            for(int j {0}; j < N; ++j) {
+                for(int k {0}; k < Cols; ++k) {
+                    m[i][j] += _data[i][k]* rhs[k][j];
+                }
+            }
+        } 
+        return m;
+    }
+    
+    Tuple operator*(const Tuple& t) {
+        if(Cols != 4) {
+            throw std::invalid_argument("error: matrix must have 4 columns to multiply with the 4 element tuple");
+        }
+        return Tuple(_data[0][0]*t.x() + _data[0][1]*t.y() + _data[0][2]*t.z() + _data[0][3]*t.w(),
+                     _data[1][0]*t.x() + _data[1][1]*t.y() + _data[1][2]*t.z() + _data[1][3]*t.w(),
+                     _data[2][0]*t.x() + _data[2][1]*t.y() + _data[2][2]*t.z() + _data[2][3]*t.w(),
+                     _data[3][0]*t.x() + _data[3][1]*t.y() + _data[3][2]*t.z() + _data[3][3]*t.w());
     }
 };
 
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -6,9 +6,12 @@ TEST(MatrixTest, EmptyInitialization) {
     Matrix<float, 2, 2> m; 
     Matrix<float, 3, 2> n;
     n = {1.0f, 3.4f, 5.1f, 0.0f, 2.0f, 1.4f};
-
     EXPECT_FLOAT_EQ(m[0][0], 0.0f);
     EXPECT_FLOAT_EQ(n[2][1], 1.4f);
+    constexpr int rows = n.rows();
+    constexpr int cols = n.cols();
+    EXPECT_EQ(rows, 3);
+    EXPECT_EQ(cols, 2);
 }
 
 TEST(MatrixTest, BracketOperator) {
@@ -45,4 +48,36 @@ TEST(MatrixTest, EqualityOperators) {
     EXPECT_FALSE(m != n);
     EXPECT_FALSE(p == q);
     EXPECT_TRUE(p != q);
+}
+
+
+TEST(MatrixTest, Multiplication) {
+    Matrix<int, 4, 4> m;
+    Matrix<int, 4, 4> n;
+    Matrix<int, 4, 3> x;
+    Matrix<int, 3, 3> y;
+    Matrix<int, 4, 4> expected;
+    Matrix<int, 4, 3> expected2;
+    m = {1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2};
+    n = {-2,1,2,3,3,2,1,-1,4,3,6,5,1,2,7,8};
+    x = {1,0,1,2,1,1,0,1,1,1,1,2};
+    y = {1,2,1,2,3,1,4,2,2};
+    expected = {20,22,50,48,44,54,114,108,40,58,110,102,16,26,46,42};
+    expected2 = {5,4,3,8,9,5,6,5,3,11,9,6};
+    auto actual = m * n;
+    auto actual2 = x * y;
+    EXPECT_TRUE(actual == expected);
+    EXPECT_TRUE(actual2 == expected2);
+}
+
+
+TEST(MatrixTest, MultiplicationWithTuple) {
+    Matrix<float, 4, 4> m;
+    m = {1.0f,2.0f,3.0f,4.0f,2.0f,4.0f,4.0f,2.0f,8.0f,6.0f,4.0f,1.0f,0.0f,0.0f,0.0f,1.0f};
+    Tuple t = Tuple::Point(1.0f, 2.0f, 3.0f);
+    Tuple expected = Tuple::Point(18.0f, 24.0f, 33.0f);
+    auto result = m * t;
+
+    EXPECT_TRUE(result == expected);
+
 }
